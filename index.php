8e62f6b96924cdc7f6ffb7fa987ba9e8b34b28ea
Dominic Vogl <dv@cccc.de>
2013-02-16 18:02:44 +0100

    [init] let's go!

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="ie6 no-js" lang="de"> <![endif]-->
+<!--[if IE 7]><html class="ie7 no-js" lang="de"> <![endif]-->
+<!--[if IE 8]><html class="ie8 no-js" lang="de"> <![endif]-->
+<!--[if IE 9]><html class="ie9 no-js" lang="de"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="de"> <!--<![endif]-->
+	<head>
+	
+		<meta charset="utf-8">	
+		
+		<title>Template Name</title>
+		
+		<!-- Insert Favicons -->
+		<link rel="shortcut icon" href="images/favicon.ico" />
+		<link rel="icon" type="image/png" href="images/favicon.png" />
+		
+		<!-- Insert CSS -->
+		<link href="css/global.css" type="text/css" rel="stylesheet" media="all" />
+		
+		<!-- Insert Javascript -->
+		<script src="js/init.js" type="text/javascript"></script>
+	
+	</head>
+	
+	<body>
+	
+		<p>Silence is golden!</p>
+	
+	</body>
+	
+</html>
